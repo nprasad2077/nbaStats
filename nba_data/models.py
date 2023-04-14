c36379775d6be72e304de0f461449ec7695b959c
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-14 14:58:48 -0500

    model create

--- nba_data/models.py
+++ nba_data/models.py
@@ -1,4 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class PlayerData(models.Model):
+    player = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    games = models.CharField(max_length=100)
+    games_started = models.CharField(max_length=100)
+    minutes_pg = models.CharField(max_length=100)
+    field_goals = models.CharField(max_length=100)
+    field_attempts = models.CharField(max_length=100)
+    field_percent = models.CharField(max_length=100)
+    three_fg = models.CharField(max_length=100)
+    three_attempts = models.CharField(max_length=100)
+    three_percent = models.CharField(max_length=100)
+    two_fg = models.CharField(max_length=100)
+    two_attempts = models.CharField(max_length=100)
+    two_percent = models.CharField(max_length=100)
+    effect_fg_percent = models.CharField(max_length=100)
+    ft = models.CharField(max_length=100)
+    fta = models.CharField(max_length=100)
+    ft_percent = models.CharField(max_length=100)
+    ORB= models.CharField(max_length=100)
+    DRB = models.CharField(max_length=100)
+    TRB = models.CharField(max_length=100)
+    AST = models.CharField(max_length=100)
+    STL = models.CharField(max_length=100)
+    BLK = models.CharField(max_length=100)
+    TOV = models.CharField(max_length=100)
+    PF = models.CharField(max_length=100)
+    PTS = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.player
 
