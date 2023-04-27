abe22c73f1a83aa728a2614ac03701a0915f08e4
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-27 13:09:34 -0500

    create player total data model

--- nba_data/models.py
+++ nba_data/models.py
@@ -34,4 +34,36 @@ class PlayerData(models.Model):
     
     def __str__(self):
         return self.name
+    
+class PlayerTotalsData(models.Model):
+    player_name = models.CharField(max_length=255, null=True)
+    age = models.IntegerField(null=True)
+    games = models.IntegerField(null=True)
+    games_started = models.IntegerField(null=True)
+    minutes_played = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    field_goals = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    field_attempts = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    field_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    three_fg = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    three_attempts = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    three_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    two_fg = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    two_attempts = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    two_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    effect_fg_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    ft = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    fta = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ft_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    ORB= models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    DRB = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    TRB = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    AST = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    STL = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    BLK = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    TOV = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    PF = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    PTS = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    team = models.CharField(max_length=30, default='')
+    season = models.IntegerField(null=True)
+    
 
