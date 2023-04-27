d8f02a331be38e2cd70c4920c37b9ad52a396a5b
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-27 13:24:40 -0500

    advanced model added

--- nba_data/models.py
+++ nba_data/models.py
@@ -47,7 +47,7 @@ class PlayerTotalsData(models.Model):
     three_fg = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     three_attempts = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     three_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
-    two_fg = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    two_fg = models.DecimalField(max_digits=5, decimal_places=3, null=True)
     two_attempts = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     two_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
     effect_fg_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
@@ -65,5 +65,42 @@ class PlayerTotalsData(models.Model):
     PTS = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     team = models.CharField(max_length=30, default='')
     season = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.player_name
+    
+    
+class PlayerAdvancedData(models.Model):
+    player_name = models.CharField(max_length=255, null=True)
+    age = models.IntegerField(null=True)
+    games = models.IntegerField(null=True)
+    minutes_played = models.IntegerField(null=True)
+    PER = models.DecimalField(max_digits=5, decimal_places=1, null=True)
+    TS_percent = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    three_p_attempt_rate = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    ft_attempt_rate = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    orb_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    drb_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    trb_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ast_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    stl_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    blk_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    tov_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    usg_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ows = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    dws = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ws = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ws_per_48 = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    obpm = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    dbpm = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    bpm = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    vorp = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    
+    def __str__(self):
+        return self.player_name
+    
+    
+        
+        
     
 
