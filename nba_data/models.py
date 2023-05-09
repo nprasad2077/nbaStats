601e320ae073d767649811a5a9cc1b3dec015f45
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-08 19:00:55 -0500

    new salary model;

--- nba_data/models.py
+++ nba_data/models.py
@@ -199,6 +199,14 @@ class PlayerPlayoffAdvancedData(models.Model):
     def __str__(self):
         return self.player_name
     
+
+class PlayerSalaryData(models.Model):
+    player_name = models.CharField(max_length=255, null=True)
+    salary = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.player_name
+    
     
         
         
